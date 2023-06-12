2a8e71926b80fd7681131dfe8c2b2916eb3972ca
marigv95 <mama_guima04@hotmail.com>
2023-06-12 16:43:03 -0300

    feat: tarefa 01 finalizada

--- users/serializers.py
+++ users/serializers.py
@@ -1,38 +1,35 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from users.models import User
 
 
 class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
 
-    email = serializers.EmailField(max_length=127)
-    username = serializers.CharField(max_length=150)
-    password = serializers.CharField(write_only=True)
-    is_employee = serializers.BooleanField(required=False, default=False)
-
+    username = serializers.CharField(
+        max_length=80,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(), message="username already taken."
+            )
+        ],
+    )
+    password = serializers.CharField(max_length=127, write_only=True)
+    email = serializers.EmailField(
+        max_length=127,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(), message="email already registered."
+            )
+        ],
+    )
+    birthdate = serializers.DateField(allow_null=True, default=None)
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    is_employee = serializers.BooleanField(allow_null=True, default=False)
     is_superuser = serializers.BooleanField(read_only=True)
 
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("email already registered.")
-        return value
-
-    def validate_username(self, value):
-        if User.objects.filter(username=value).exists():
-            raise serializers.ValidationError("username already taken.")
-        return value
-
     def create(self, validated_data):
-        password = validated_data.pop("password")
-        is_employee = validated_data.pop("is_employee", False)
-        is_superuser = False
-
-        if is_employee:
-            is_superuser = True
-
-        user = User(**validated_data)
-        user.set_password(password)
-        user.is_employee = is_employee
-        user.is_superuser = is_superuser
-        user.save()
+        validated_data["is_superuser"] = validated_data.get("is_employee", False)
+        user = User.objects.create_user(**validated_data)
         return user
